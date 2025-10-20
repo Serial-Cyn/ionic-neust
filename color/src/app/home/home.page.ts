57e6113bc62a3073c4173b20f5adba815f657370
Christian Mamplata <201923244+Serial-Cyn@users.noreply.github.com>
2025-10-20 11:02:35 +0800

    feat: update home page to display color selection functionality

--- color/src/app/home/home.page.ts
+++ color/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonCol, IonRow, CommonModule, FormsModule],
 })
 export class HomePage {
   constructor() {}
+
+  selectedColor: string = "black";
+
+  colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
+
+  selectColor(color: string) {
+    this.selectedColor = color;
+  }
 }
